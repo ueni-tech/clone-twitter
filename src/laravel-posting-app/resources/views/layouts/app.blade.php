8028c6a8ec8d22b7751f4a3c04daae2f293a2273
uenishi <ueni.tech@gmail.com>
2024-04-14 17:25:38 +0900

    #3 レイアウトの共通化

--- src/laravel-posting-app/resources/views/layouts/app.blade.php
+++ src/laravel-posting-app/resources/views/layouts/app.blade.php
@@ -1,36 +1,69 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="ja">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+</head>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+<body>
+    <div class="wrapper">
+        @include('layouts.header')
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+        <main>
+            <div class="container">
+                <h1 class="fs-2 my-3">@yield('title')</h1>
+                @yield('content')
+            </div>
+        </main>
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+        @include('layouts.footer')
+    </div>
+</body>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+
 </html>
+
+
+{{--
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
+        @include('layouts.navigation')
+
+        <!-- Page Heading -->
+        @if (isset($header))
+        <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                {{ $header }}
+            </div>
+        </header>
+        @endif
+
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+
+</html>
+--}}
--- src/laravel-posting-app/resources/views/posts/show.blade.php
+++ src/laravel-posting-app/resources/views/posts/show.blade.php
@@ -1,57 +1,25 @@
-<!DOCTYPE html>
-<html lang="ja">
+@extends('layouts.app')
+@section('title', '投稿詳細')
+@section('content')
+@if(session('flash_message'))
+<p>{{ session('flash_message') }}</p>
+@endif
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>投稿詳細</title>
-</head>
+<a href="{{ route('posts.index') }}">&lt; 戻る</a>
 
-<body>
-  <header>
-    <nav>
-      <a href="{{ route('posts.index') }}">投稿アプリ</a>
+<article>
+  <h2>{{ $post->title }}</h2>
+  <p>{{ $post->content }}</p>
 
-      <ul>
-        <li>
-          <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
-          <form id="logout-form" action="{{ route('logout') }}" method="POST">
-            @csrf
-          </form>
-        </li>
-      </ul>
-    </nav>
-  </header>
+  @if($post->user_id === Auth::id())
+  <a href="{{ route('posts.edit', $post) }}">編集</a>
 
-  <main>
-    <h1>投稿詳細</h1>
+  <form action="{{ route('posts.destroy', $post) }}" method="POST" onsubmit="return confirm('本当に削除してもよろしいですか？')">
+    @csrf
+    @method('DELETE')
 
-    @if(session('flash_message'))
-    <p>{{ session('flash_message') }}</p>
-    @endif
-
-    <a href="{{ route('posts.index') }}">&lt; 戻る</a>
-
-    <article>
-      <h2>{{ $post->title }}</h2>
-      <p>{{ $post->content }}</p>
-
-      @if($post->user_id === Auth::id())
-      <a href="{{ route('posts.edit', $post) }}">編集</a>
-
-      <form action="{{ route('posts.destroy', $post) }}" method="POST" onsubmit="return confirm('本当に削除してもよろしいですか？')">
-        @csrf
-        @method('DELETE')
-
-        <button type="submit">削除</button>
-      </form>
-      @endif
-    </article>
-  </main>
-
-  <footer>
-    <p>&copy; 投稿アプリ All rights reserved.</p>
-  </footer>
-</body>
-
-</html>
+    <button type="submit">削除</button>
+  </form>
+  @endif
+</article>
+@endsection
