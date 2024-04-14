6491341abddbf4ccce3aba17c5722f985ab67111
uenishi <ueni.tech@gmail.com>
2024-04-14 11:27:44 +0900

    #3 投稿作成機能実装

---
+++ src/laravel-posting-app/app/Http/Controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Auth::user()->posts()->orderBy('created_at', 'desc')->get();
+        return view('posts.index', compact('posts'));
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts.show', compact('post'));
+    }
+}
---
+++ src/laravel-posting-app/tests/Feature/PostTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    use RefreshDatabase;
+    public function test_guest_user_cannot_access_posts_index()
+    {
+        $respose = $this->get(route('posts.index'));
+        $respose->assertRedirect(route('login'));
+    }
+    
+    public function test_user_can_access_posts_index()
+    {
+        $user = User::factory()->create();
+        $post = Post::factory()->create(['user_id' => $user->id]);
+
+        $response = $this->actingAs($user)->get(route('posts.index'));
+
+        $response->assertStatus(200);
+        $response->assertSee($post->title);
+    }
+
+    public function test_guest_cannot_access_posts_show()
+    {
+        $user = User::factory()->create();
+        $post = Post::factory()->create(['user_id' => $user->id]);
+
+        $response = $this->get(route('posts.show', $post));
+        $response->assertRedirect(route('login'));
+    }
+
+    public function test_user_can_access_posts_show()
+    {
+        $user = User::factory()->create();
+        $post = Post::factory()->create(['user_id' => $user->id]);
+
+        $response = $this->actingAs($user)->get(route('posts.show', $post));
+
+        $response->assertStatus(200);
+        $response->assertSee($post->title);
+    }
+}
